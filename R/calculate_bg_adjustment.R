c219d097a53d208fe4dd89e28d161fd885a2577c
Valentin Voillet <valentin.voillet@gmail.com>
2026-01-06 13:51:29 +1100

    Edit calculate_bg_adjustment.R : ANTIGEN or STIM colnames

--- R/calculate_bg_adjustment.R
+++ R/calculate_bg_adjustment.R
@@ -32,18 +32,34 @@ calculate_bg_adjustment <- function(x,
   # Ensure input is a data frame
   if (!is.data.frame(x)) stop("Input 'x' must be a data.frame or tibble.")
 
+  # 0. Check for stimulation column (ANTIGEN or STIM)
+  col_options <- c("ANTIGEN", "STIM")
+  stim_col <- intersect(col_options, colnames(x))
+
+  if (length(stim_col) == 0) {
+    stop("Required column 'ANTIGEN' or 'STIM' not found in the dataset.")
+  } else {
+    stim_col <- stim_col[1] # Use the first match found
+  }
+
   # 1. Extract Background (Negative Controls)
   # We create a temporary 'SAMPLE_ID' to match controls to stimulated samples
   dt_bg <- x %>%
-    dplyr::filter(ANTIGEN == neg_id) %>%
+    dplyr::filter(!!dplyr::sym(stim_col) == neg_id) %>%
     dplyr::mutate(SAMPLE_ID = do.call(paste, c(dplyr::select(., dplyr::all_of(group_cols)), sep = "_"))) %>%
     dplyr::rename(NSUB_NEG = NSUB, CYTNUM_NEG = CYTNUM) %>%
     dplyr::select(SAMPLE_ID, NSUB_NEG, CYTNUM_NEG)
 
   # 2. Extract Stimulated Samples
   dt_stim <- x %>%
-    dplyr::filter(ANTIGEN != neg_id) %>%
-    dplyr::mutate(SAMPLE_ID = do.call(paste, c(dplyr::select(., dplyr::all_of(group_cols)), sep = "_")))
+    dplyr::filter(!!dplyr::sym(stim_col) != neg_id) %>%
+    dplyr::mutate(SAMPLE_ID = do.call(paste, c(dplyr::select(., dplyr::all_of(group_cols)), sep = "_"))) %>%
+    dplyr::select(
+      SAMPLE_ID,
+      dplyr::all_of(group_cols),
+      dplyr::all_of(stim_col),
+      NSUB,
+      CYTNUM)
 
   # 3. Merge and Calculate Adjusted Percentages
   final_dt <- dt_stim %>%
